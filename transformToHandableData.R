c5bfe68478b74ebea19aaab82a44ab9bf21495f6
SebastianFish <duebasti@gmail.com>
2019-04-11 15:53:26 +0200

    if Nachweisgrenze -> NA only 555 in winddirection changed to NA changed inputfolder of Data dor clean Folderstructuree

--- transformToHandableData.R
+++ transformToHandableData.R
@@ -1,14 +1,20 @@
 library(openair)
 library(reshape2)
 library("gplots")
-f.list <- list.files(path = "X:/p3_working/SDuesing-home/Data/LfULG/StickoxidProjekt/Data/",pattern=".csv")
+f.list <- list.files(path = "X:/p3_working/SDuesing-home/Data/LfULG/StickoxidProjekt/Data/Stationen",pattern=".csv")
 
 table.final <- NULL
 for (i in 1:length(f.list)){
-  table <- read.csv(paste("./Data/",f.list[i],sep=""),quote="'",sep=";",header = TRUE,dec = ".",na.strings = c("-999.0","  -999.00"),fill = TRUE,stringsAsFactors = T,strip.white = TRUE)
+  table <- read.csv(paste("./Data/Stationen/",f.list[i],sep=""),quote="'",sep=";",header = TRUE,dec = ".",na.strings = c("-999.0","  -999.00"),fill = TRUE,stringsAsFactors = T,strip.white = TRUE)
+  if(names(table)[length(names(table))]=="Nachweisgrenze"){
+    
+  }else{
+    table$Nachweisgrenze <-NA
+  }
   table.final <- rbind(table.final,table)
 }
 table.final[table.final==-999]<-NA
+
 
 names(table.final)[3]<-"date"
 table.final$date <- as.POSIXct(strptime(as.character(table.final$date),format = "%Y%m%d",tz="UTC"),tz="UTC") #converts Datum to date
@@ -23,7 +29,8 @@ final.vector[1:nrow(table.final.new)%%24==i]  <- as.vector(table.final.new[1:nro
 
 table.final.final <- cbind(table.final.new[,c(1:3,28)],unlist(final.vector))
 names(table.final.final)[c(2,4,5)]<-c("variable","Nachweisgrenze","value")
-
+table.final.final$value[table.final.final$value<table.final.final$Nachweisgrenze]<-NA
 final <- dcast(table.final.final,Station + date ~ variable, value.var = "value")
-write.table(final,"StationOverview.dat",col.names = T,row.names = F,append=F,quote=F,sep="\t")
+final$Windrichtung[final$Windrichtung==555]<-NA
+write.table(final,"StationOverview_20190411.dat",col.names = T,row.names = F,append=F,quote=F,sep="\t")
 
